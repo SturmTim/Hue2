efa505b57249139191eff2ed97e1669e276f682c
Tim STURM <tsturm18@htl-grieskirchen.local>
2020-09-29 12:07:34 +0200

    fileTester teil 1

--- Beispiel3/src/pos3/tsturm18/NumberTester.java
+++ Beispiel3/src/pos3/tsturm18/NumberTester.java
@@ -5,6 +5,14 @@
  */
 package pos3.tsturm18;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author tsturm18
@@ -31,6 +39,48 @@ public class NumberTester {
         this.palindromeTester = palindromeTester;
     }
     public void testFile(){
+        File file =new File(pfad);
+        
+        if(!file.isFile()){
+            System.exit(0);
+        }
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            
+            String zeile = "";
+            zeile = br.readLine();
+            int zeilenZahl = Integer.parseInt(zeile);
+            setOddEvenTester( (i) -> (i%2) == 1 );
+            
+            do
+            {
+                zeile=br.readLine();
+                String[] s = zeile.split(" ");
+                if(s.length==2){
+                    switch (Integer.parseInt(s[0])){
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        default: System.out.println("Keine Akzeptierte Zahl");
+                    }
+                }
+                else{
+                    System.out.println("Nich genügend Zahlen in dieser Zeile");
+                }
+            }
+            while (zeile != null);
+            
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         
     }
 }
